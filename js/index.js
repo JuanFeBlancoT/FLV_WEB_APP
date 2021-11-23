8cadc1a62914a5e9e1e8b14c4b9462981cfe086d
JuanFeBlancoT <juanfeblancot@gmail.com>
2021-11-23 18:24:54 -0500

    Initial commit

---
+++ js/index.js
@@ -0,0 +1,9 @@
+var continueBtn = document.getElementById("mainBtn");
+
+
+
+function loadIdex2(){
+    window.location.href = '/html/index2.html';
+}
+
+continueBtn.addEventListener('click', loadIdex2);
