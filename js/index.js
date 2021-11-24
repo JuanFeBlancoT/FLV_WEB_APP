1d28403cdca3a913e1996baddeafe7193d0b1833
UNDERSTRAKER <felipepadilla473@gmail.com>
2021-11-24 00:52:23 -0500

    Agregar inicio del modulo biblioteca y parte de la interfaz

--- js/index.js
+++ js/index.js
@@ -1,9 +1,14 @@
 var continueBtn = document.getElementById("mainBtn");
+var continueBtn = document.getElementById("mainBtnL");
 
 
 
 function loadIdex2(){
     window.location.href = '/html/index2.html';
 }
+function loadLibrary2(){
+    window.location.href = '/html/Blibioteca/inicio2.html';
+}
 
-continueBtn.addEventListener('click', loadIdex2);
+/*continueBtn.addEventListener('click', loadIdex2);
+continueBtn.addEventListener('click', loadLibrary2);*/
